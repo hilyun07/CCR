03800aa8fa25b59ee9f16954a4e3d1283a51542f
Jaehyung Lee <jaehyung.lee@sf.snu.ac.kr>
2023-01-16 16:07:47 +0900

    wip

---
+++ examples/twosite/Main0.v
@@ -0,0 +1,43 @@
+Require Import Coqlib.
+Require Import ITreelib.
+Require Import ModSem.
+Require Import Skeleton.
+Require Import Any.
+From compcert Require Import Values.
+
+Set Implicit Arguments.
+
+
+
+Section PROOF.
+
+  (***
+    f(n) := if (n == 0) then 0 else (n + g(n-1))
+  ***)
+  Definition fF: list val -> itree Es val :=
+    fun varg =>
+      `n: Z <- (pargs [Tint] varg)?;;
+      _ <- trigger (Syscall "print" [n]↑ top1);;
+      `pid:Z <- ccallU "Spawn" ("g", [Vint 2]);;
+      _ <- trigger (Syscall "print" [pid]↑ top1);;
+      Ret (Vint n).
+  
+  Definition gF: list val -> itree Es val :=
+    fun varg =>
+      `n: Z <- (pargs [Tint] varg)?;;
+      _ <- trigger (Syscall "print" [n]↑ top1);;
+      Ret (Vint n).
+
+  Definition FSem: ModSem.t := {|
+    ModSem.fnsems := [("f", cfunU fF); ("g", cfunU gF)];
+    ModSem.mn := "F";
+    ModSem.initial_st := tt↑;
+  |}
+  .
+
+  Definition F: Mod.t := {|
+    Mod.get_modsem := fun _ => FSem;
+    Mod.sk := [("f", Sk.Gfun)];
+  |}
+  .
+End PROOF.
