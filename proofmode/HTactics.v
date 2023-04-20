2a1b1cd5fc7654c1e83aa00ff7ccbfcd8364427a
Samuel Arsac <samuel.arsac@ens-paris-saclay.fr>
2023-04-20 13:04:36 +0900

    Require equality of arguments for safe_sim_itree_call

--- proofmode/HTactics.v
+++ proofmode/HTactics.v
@@ -85,14 +85,15 @@ Section SIM.
       _safe_sim_itree r g RR i_src0 i_tgt0 w0 (st_src0, (Ret v_src)) (st_tgt0, (Ret v_tgt))
   | safe_sim_itree_call
       i_src0 i_tgt0 w st_src0 st_tgt0
-      fn varg k_src k_tgt
+      fn varg0 varg1 k_src k_tgt
+      (EQVARG: varg0 = varg1)
       (SIM: exists w0,
           (<<WF: wf w0 (st_src0, st_tgt0)>>) /\
           (<<K: forall w1 vret st_src1 st_tgt1 (WLE: le w0 w1) (WF: wf w1 (st_src1, st_tgt1)),
               g _ _ RR true true w (st_src1, k_src vret) (st_tgt1, k_tgt vret)>>))
     :
-      _safe_sim_itree r g RR i_src0 i_tgt0 w (st_src0, trigger (Call fn varg) >>= k_src)
-                      (st_tgt0, trigger (Call fn varg) >>= k_tgt)
+      _safe_sim_itree r g RR i_src0 i_tgt0 w (st_src0, trigger (Call fn varg0) >>= k_src)
+                      (st_tgt0, trigger (Call fn varg1) >>= k_tgt)
   | safe_sim_itree_syscall
       i_src0 i_tgt0 w0 st_src0 st_tgt0
       fn varg rvs k_src k_tgt
